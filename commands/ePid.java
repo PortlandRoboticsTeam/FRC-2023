b25d4182fc2b37498f4ecfbd89e503e11c18fa7a
imhassett <94932727+imhassett@users.noreply.github.com>
2023-10-19 21:09:29 -0400

    Add files via upload

---
+++ commands/ePid.java
@@ -0,0 +1,46 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import frc.robot.Constants;
+import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.subsystems.elbow;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+/** An example command that uses an example subsystem. */
+public class ePid extends CommandBase {
+    @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+    private final elbow m_elbow;
+
+    
+    public ePid(elbow elbow) {
+      m_elbow = elbow;
+      // Use addRequirements() here to declare subsystem dependencies.
+      addRequirements(elbow);
+    }
+
+    // Called when the command is initially scheduled.
+    @Override
+    public void initialize() {
+      m_elbow.getController().enableContinuousInput(-180, 180);
+    }
+
+    // Called every time the scheduler runs while the command is scheduled.
+    @Override
+    public void execute() {
+        m_elbow.useOutput(elbow.optimise(m_elbow.getMeasurement(),m_elbow.getController().calculate(m_elbow.getMeasurement(), Constants.EAngels[m_elbow.position])), Constants.EAngels[m_elbow.position]);
+    }
+
+    // Called once the command ends or is interrupted.
+    @Override
+    public void end(boolean interrupted) {}
+
+    // Returns true when the command should end.
+    @Override
+    public boolean isFinished() {
+      return false;
+    }
+}
---
+++ commands/sPid.java
@@ -0,0 +1,45 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import frc.robot.Constants;
+import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.subsystems.shoulder;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+/** An example command that uses an example subsystem. */
+public class sPid extends CommandBase {
+    @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+    private final shoulder m_shoulder;
+    
+    public sPid(shoulder shoulder) {
+      m_shoulder = shoulder;
+      // Use addRequirements() here to declare subsystem dependencies.
+      addRequirements(shoulder);
+      m_shoulder.getController().enableContinuousInput(-180, 180);
+    }
+
+    // Called when the command is initially scheduled.
+    @Override
+    public void initialize() {
+    }
+
+    // Called every time the scheduler runs while the command is scheduled.
+    @Override
+    public void execute() {
+       m_shoulder.useOutput(shoulder.optimise(m_shoulder.getMeasurement(),  m_shoulder.getController().calculate(m_shoulder.getMeasurement(), Constants.SAngels[m_shoulder.position])), Constants.SAngels[m_shoulder.position]);
+    }
+
+    // Called once the command ends or is interrupted.
+    @Override
+    public void end(boolean interrupted) {}
+
+    // Returns true when the command should end.
+    @Override
+    public boolean isFinished() {
+      return false;
+    }
+}
